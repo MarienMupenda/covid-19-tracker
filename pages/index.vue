80d962e4bfbdd3396d0e0e6c22bd264b0bcfc66f
Marien Mupenda <marien.mupenda@gmail.com>
2022-04-19 18:09:28 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="">
+    <h1>La façon la plus simple de recuperer son releve de cote simple</h1>
+    <button>
+      <NuxtLink to="about">test</NuxtLink>
+    </button>
+   </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
